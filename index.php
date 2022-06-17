c7982e6248542ee9872d2f6d24a7d5c6cc376bd3
shankar5885 <34203987+shankar5885@users.noreply.github.com>
2022-06-16 20:45:33 -0700

    Update index.php

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
 	
    <body>
       
-      <h2>Enter Username and Password To Validate</h2> 
+      <h2>Enter Username and Password</h2> 
       <div class = "container form-signin">
          
          <?php
